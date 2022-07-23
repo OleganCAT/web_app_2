caee55d8fd71cf03f0af1bf4c40291aeeb1aa090
Oleh Foia <oleganfoya@gmail.com>
2022-07-23 23:08:23 +0300

    add struct user

--- main.go
+++ main.go
@@ -5,8 +5,25 @@ import (
 	"net/http"
 )
 
+type User struct {
+	Name                string
+	Age                 uint16
+	money               int16
+	avGrades, happiness float64
+}
+
+func (u User) getAllInfo() string {
+	return fmt.Sprintf("User name is: %s. He is %d and he has money equal %d", u.Name, u.Age, u.money)
+}
+
+func (u *User) setNewName(newName string) {
+	u.Name = newName
+}
+
 func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Go super easy!")
+	bob := User{"Alex", 25, -50, 4.2, 0.7}
+	bob.setNewName("Alex")
+	fmt.Fprintf(w, bob.getAllInfo())
 }
 
 func contactsPage(w http.ResponseWriter, r *http.Request) {
