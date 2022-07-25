2ed1cf32091e7a0e2568cb572ccd501d588fd240
Oleh Foia <oleganfoya@gmail.com>
2022-07-26 01:30:16 +0300

    ststic css

--- main.go
+++ main.go
@@ -6,38 +6,21 @@ import (
 	"net/http"
 )
 
-type User struct {
-	Name                string
-	Age                 uint16
-	Money               int16
-	AvGrades, Happiness float64
-	Hobbies             []string
+func index(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/index.html", "templates/header.html", "templates/footer.html")
+
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+	t.ExecuteTemplate(w, "index", nil)
 }
 
-func (u User) getAllInfo() string {
-	return fmt.Sprintf("User name is: %s. He is %d and he has money equal %d", u.Name, u.Age, u.Money)
-}
-
-func (u *User) setNewName(newName string) {
-	u.Name = newName
-}
-
-func homePage(w http.ResponseWriter, r *http.Request) {
-	bob := User{"Bob", 25, -50, 4.2, 0.7, []string{"Football", "Skate", "Dance"}}
-	tmpl, _ := template.ParseFiles("templates/homePage.html")
-	tmpl.Execute(w, bob)
-}
-
-func contactsPage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Contacts page!")
-}
-
-func handelRequest() {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/contacts/", contactsPage)
-	http.ListenAndServe("localhost:8080", nil)
+func handelFunc() {
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":8080", nil)
 }
 
 func main() {
-	handelRequest()
+	handelFunc()
 }
