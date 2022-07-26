3099b645d2f62c1f94d41be3951b781db3077495
Oleh Foia <oleganfoya@gmail.com>
2022-07-26 18:50:56 +0300

    new DB and

--- main.go
+++ main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"html/template"
 	"net/http"
 )
+
+type Article struct {
+	Id                     uint16
+	Title, Anons, FullText string
+}
+
+var posts = []Article{}
 
 func index(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("templates/index.html", "templates/header.html", "templates/footer.html")
@@ -12,13 +21,68 @@ func index(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		fmt.Fprintf(w, err.Error())
 	}
-	t.ExecuteTemplate(w, "index", nil)
+	db, err := sql.Open("mysql", "root:nhf[!)!hfp@tcp(127.0.0.1:3306)/golangdb")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	res, err := db.Query("SELECT * FROM `articles`")
+	if err != nil {
+		panic(err)
+	}
+	posts = []Article{}
+	for res.Next() {
+		var post Article
+		err := res.Scan(&post.Id, &post.Title, &post.Anons, &post.FullText)
+		if err != nil {
+			panic(err)
+		}
+		posts = append(posts, post)
+	}
+
+	t.ExecuteTemplate(w, "index", posts)
+}
+
+func create(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/create.html", "templates/header.html", "templates/footer.html")
+
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+	t.ExecuteTemplate(w, "create", nil)
+}
+
+func saveArticle(w http.ResponseWriter, r *http.Request) {
+	title := r.FormValue("title")
+	anons := r.FormValue("anons")
+	full_text := r.FormValue("full_text")
+
+	if title == "" || anons == "" || full_text == "" {
+		fmt.Fprintf(w, "Заполнены не все данные")
+	} else {
+		db, err := sql.Open("mysql", "root:nhf[!)!hfp@tcp(127.0.0.1:3306)/golangdb")
+		if err != nil {
+			panic(err)
+		}
+		defer db.Close()
+
+		insert, err := db.Query(fmt.Sprintf("INSERT INTO `articles` (`title`, `anons`, `full_text`) VALUES ('%s', '%s', '%s')", title, anons, full_text))
+		if err != nil {
+			fmt.Fprintf(w, err.Error())
+		}
+		defer insert.Close()
+
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	}
 }
 
 func handelFunc() {
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+	http.HandleFunc("/saveArticle/", saveArticle)
+	http.HandleFunc("/create/", create)
 	http.HandleFunc("/", index)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8888", nil)
 }
 
 func main() {
